4f617d468b1f5ecb5ced4a4da079c494509349f9
Anggit1 <anggitrefiyan726@gmail.com>
2022-03-10 13:33:21 +0700

    upload modul 5

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,17 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('index' , [
+    "title" => "Beranda"
+   ]);
 });
 
 Route::get('/about', function () {
     return view('about' , [
+        "title" => "About",
         "nama" => "Anggit Refiyan",
         "email" => "Anggit@smktelkom-pwt.sch.id",
-        "gambar" => "Anggit.jpg"
+        "gambar" => "Anggit Refiyan.jpg"
     ]);
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery' , [
+        "title" => "Gallery"
+    ]);
+});
+
+Route::resource('/contacts', ContactController::class );
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 });
